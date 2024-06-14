35ebaca4a1fc02265cccfa6f5f1ecf6b3efd879e
Viktor Talashko <viktortalashko@gmail.com>
2024-06-14 21:35:42 +0300

    make build

---
+++ index.f966ef1c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.top="".concat((l-n)/2,"px"),t.style.left="".concat((c-i)/2,"px");
+//# sourceMappingURL=index.f966ef1c.js.map
